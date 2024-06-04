ab7ef4e0ffd27ca01ce13f9447ff32e7e437ed34
Vjosa Curri <114615502+vjosac@users.noreply.github.com>
2024-06-04 09:43:46 +0200

    Setup AccompanimentMenuScreen

---
+++ app/src/main/java/com/example/lunchtray/ui/AccompanimentMenuScreen.kt
@@ -0,0 +1,44 @@
+package com.example.lunchtray.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lunchtray.R
+import com.example.lunchtray.datasource.DataSource
+import com.example.lunchtray.model.MenuItem
+
+@Composable
+fun AccompanimentMenuScreen(
+    options: List<MenuItem.AccompanimentItem>,
+    onCancelButtonClicked: () -> Unit,
+    onNextButtonClicked: () -> Unit,
+    onSelectionChanged: (MenuItem.AccompanimentItem) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    BaseMenuScreen(
+        options = options,
+        onCancelButtonClicked = onCancelButtonClicked,
+        onNextButtonClicked = onNextButtonClicked,
+        onSelectionChanged = onSelectionChanged as (MenuItem) -> Unit,
+        modifier = modifier
+    )
+}
+
+
+@Preview
+@Composable
+fun AccompanimentMenuPreview() {
+    AccompanimentMenuScreen(
+        options = DataSource.accompanimentMenuItems,
+        onCancelButtonClicked = {},
+        onNextButtonClicked = {},
+        onSelectionChanged = {},
+        modifier = Modifier
+            .padding(dimensionResource(R.dimen.padding_medium))
+            .verticalScroll(rememberScrollState())
+    )
+}
